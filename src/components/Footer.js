3d221b3e606f3c5c7aebdc6f906285ff81662f62
Hikmet Uygur <uygurhikmet@gmail.com>
2023-08-31 15:32:11 +0300

    Logo changed, footer height bug fixed.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -23,7 +23,7 @@ const CustomLink = ({ href, title, className = '' }) => {
 const Footer = () => {
   return (
     <footer className='w-full border-t-2 border-solid border-dark text-lg font-medium'>
-      <Layout className='py- flex items-center justify-between'>
+      <Layout className='py-6 flex items-center justify-between'>
         <span>{new Date().getFullYear()} &copy; All Rights Reserved.</span>
         <div className='flex items-center'>
           Build with <span className='px-1 text-2xl text-primary'>&#9825;</span>{' '}
